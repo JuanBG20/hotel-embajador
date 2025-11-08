5290e3593bb9e2accf4f1900394716af5fe61d35
JuanBG20 <juanbgalvan.19@gmail.com>
2025-11-07 21:01:40 -0300

    Fix image error

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,10 +4,7 @@ import { defineCollection, z } from 'astro:content';
 const habitaciones = defineCollection({
   schema: z.object({
     roomType: z.string(),
-    srcRoomImage: z.string().transform(async (path) => {
-      const { getImage } = await import('astro:assets');
-      return await getImage({ src: path, alt: 'Imagen' });
-    }), // URL de la imagen
+    srcRoomImage: z.string(), // URL de la imagen
     summary: z.string(),
     order: z.number().optional() // Orden de la habitacion
   })
